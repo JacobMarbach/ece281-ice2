c160917ab86a715b725bb6dffbf3ade6d72e10ab
JacobMarbach <C26Jacob.Marbach@afacademy.af.edu>
2024-01-29 15:35:37 -0700

    This is my submission after completing ICE2

--- src/hdl/halfAdder_tb.vhd
+++ src/hdl/halfAdder_tb.vhd
@@ -89,7 +89,7 @@ begin
 		i_B     => w_sw0,
 		o_S     => w_led0, -- no comma on LAST one
 		-- TODO:  map Cout 
-		o_Cout     => w_led1
+		o_Cout  => w_led1
 	);
 
 	-- CONCURRENT STATEMENTS ----------------------------
